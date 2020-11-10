01eb478bb702b43e568b48778d32d60d2566890c
Duica Marius <duica.marius.duica@mta.ro>
2020-11-10 16:11:22 +0200

    Implementare Pretzel si Strudel Am adaugat parametrii, niste getere si constructorii
    Signed-off-by: Duica Marius <duica.marius.duica@mta.ro>

--- src/backery_pck/Pretzel.java
+++ src/backery_pck/Pretzel.java
@@ -1,4 +1,26 @@
 package backery_pck;
 
 public class Pretzel implements Product {
+    private int pretzelPrice;
+    private int pretzelQuantity;
+
+    public Pretzel(int breadPrice, int breadQuantity) {
+        this.pretzelPrice = breadPrice;
+        this.pretzelQuantity = breadQuantity;
+    }
+
+    @Override
+    public void nutrition() {
+        System.out.println("Strudel delicioas si gustoas cu pretul" + this.pretzelPrice + " si cantitatea "+this.pretzelQuantity+"!\n");
+    }
+
+    @Override
+    public int getPrice() {
+        return this.pretzelPrice;
+    }
+
+    @Override
+    public int getQuantity() {
+        return this.pretzelQuantity;
+    }
 }
