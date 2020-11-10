2c7a57e2b3a0793f5b6bccfaad586e91c9813bfc
gutiu <gutiudan@yahoo.com>
2020-11-10 13:34:45 +0200

    Adaugare clase loaf si bread
    Am adaugat clasele respective cu constructori si mostenire
    Am schimbat paine cu franzela
    
    Signed-off-by: gutiu <gutiudan@yahoo.com>

--- src/backery_pck/Loaf.java
+++ src/backery_pck/Loaf.java
@@ -1,4 +1,26 @@
 package backery_pck;
 
 public class Loaf implements Product{
+    private int loafPrice;
+    private int loafQuantity;
+
+    public Loaf(int loafPrice, int loafQuantity) {
+        this.loafPrice = loafPrice;
+        this.loafQuantity = loafQuantity;
+    }
+
+    @Override
+    public void nutrition() {
+        System.out.println("Franzela delicioasa si gustoasa cu pretul" + loafPrice + " si cantitatea "+loafQuantity+"!\n");
+    }
+
+    @Override
+    public int getPrice() {
+        return loafPrice;
+    }
+
+    @Override
+    public int getQuantity() {
+        return loafQuantity;
+    }
 }
